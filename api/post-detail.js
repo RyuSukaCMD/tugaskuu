b6422267e7d81408090641dfa9770b398baba10d
RyuSukaCMD <regyseptiawan47@gmail.com>
2026-08-06 07:10:47 +0000

    fix: update server helper imports

--- api/post-detail.js
+++ api/post-detail.js
@@ -1,4 +1,4 @@
-import supabase from './db-client.js';
+import supabase from '../server/db-client.js';
 import { handleOptions, cors, getAuthUser, enrichPosts } from '../server/helpers.js';
 
 export default async function handler(req, res) {
